fd5de23bf1d5c1eb2dd0a9cc6887d65c14463f54
sean1295 <dcs3zc@virginia.edu>
2025-08-18 00:16:28 -0400

    uncomment prepare_dataset

--- scripts/prepare_dataset.sh
+++ scripts/prepare_dataset.sh
@@ -5,19 +5,19 @@ echo "Activating conda environment..."
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate CLASS_robomimic
 
-# # Download dataset for square task
-# python robomimic/scripts/download_datasets.py \
-# --tasks square \
-# --dataset_types ph \
-# --hdf5_types low_dim 
+# Download dataset for square task
+python robomimic/scripts/download_datasets.py \
+--tasks square \
+--dataset_types ph \
+--hdf5_types low_dim 
 
-# # Convert dataset states to observations with camera images
-# python robomimic/scripts/dataset_states_to_obs.py \
-# --dataset ./datasets/square/ph/low_dim_v15.hdf5 \
-# --output_name image_v15.hdf5 \
-# --camera_names agentview \
-# --camera_height 256 \
-# --camera_width 256
+# Convert dataset states to observations with camera images
+python robomimic/scripts/dataset_states_to_obs.py \
+--dataset ./datasets/square/ph/low_dim_v15.hdf5 \
+--output_name image_v15.hdf5 \
+--camera_names agentview \
+--camera_height 256 \
+--camera_width 256
 
 # Add absolute actions to the dataset
 python robomimic/scripts/conversion/robosuite_add_absolute_actions.py \
